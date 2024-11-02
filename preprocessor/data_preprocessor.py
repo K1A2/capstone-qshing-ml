c9909a4e86c32c3ccbdf76bcc42bd28f1d41cd14
K1A2 <jckim0307@gmail.com>
2024-11-02 21:28:04 +0900

    feat: url, html2text 추출 추가

--- preprocessor/data_preprocessor.py
+++ preprocessor/data_preprocessor.py
@@ -1,8 +1,12 @@
 import sqlite3
 import pandas as pd
 
+from html2text import HTML2Text
+from bs4 import BeautifulSoup
+
 import os
 import argparse
+from tqdm import tqdm
 
 from logger.main_logger import MainLogger
 
@@ -28,3 +32,25 @@ class DataPreprocessor:
         self.raw_data = pd.read_sql("SELECT url, html FROM data", con, index_col=None)
 
         self.logger.debug(f'raw data:\n{self.raw_data}', self.gpu)
+
+        urls, contents = self.__extract_data()
+
+    def __extract_data(self):
+        urls, contents = [], []
+
+        converter = HTML2Text()
+
+        for i in tqdm(range(self.raw_data.shape[0]), desc='extracting data'):
+            html = self.raw_data.iloc[i]['html']
+            contents.append(converter.handle(html))
+
+            soup = BeautifulSoup(html, 'html.parser')
+            url_parts = []
+            for a_tag in soup.find_all('a', href=True):
+                url_parts.append(a_tag['href'])
+            for a_tag in soup.find_all('link', href=True):
+                url_parts.append(a_tag['href'])
+            urls.append(url_parts)
+
+        self.logger.debug(f'urls: {len(urls)}\tcontents: {len(contents)}', self.gpu)
+        return urls, contents
